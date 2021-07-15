6788afdd47a9ebd57a20aee3c5dc421e3e8cf1f1
Min-Ho Lee <minho.lee0716@gmail.com>
2021-07-15 23:43:31 +0900

    인증 Decorator 함수

--- app.py
+++ app.py
@@ -4,11 +4,35 @@ from flask.json import JSONEncoder
 from flask      import (
     Flask,
     request,
+    Response,
     jsonify,
     current_app
 )
 
-import bcrypt
+import bcrypt, jwt
+from functools import wraps
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        access_token = request.headers.get('Authorization')
+        if access_token is not None:
+            try:
+                payload = jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], 'HS256')
+            except jwt.InvalidTokenError:
+                payload = None
+
+            if payload is None: return Response(status=401)
+
+            user_id = payload['user_id']
+            g.user_id = user_id
+            g.user = get_user_info(user_id) if user_id else None
+
+        else:
+            return Response(status=401)
+
+        return f(*args, **kwargs)
+    return decorated_function
 
 '''
 Default JSON encoder는 set를 JSON으로 변환할 수 없다.
